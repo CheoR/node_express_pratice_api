918091a1c839922cd418eea76927c18b7b30f3ba
CheoR <cheo.roman+github@gmail.com>
2021-07-29 09:06:53 -0500

    build: setup to use remote mongodb. View via Atlas.

--- server.jsx
+++ server.jsx
@@ -1,8 +1,31 @@
+require('dotenv').config();
+
 import express from 'express';
 import routes from './src/routes/crmRoutes';
+// import mongoose from 'mongoose';
 
+// const { MongooseClient } = require('mongodb');
 const app = express();
 const PORT = 3000;
+
+const uri = `mongodb+srv://user:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.1j6ce.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose connection
+// mongoose.Promise = global.Promise
+// mongoose.conncect('mongodb://localhost/CRMdb', {
+//   useNewUrlParsr: true,
+//   useUnifiedTopology: true
+// });
+
+/*
+Middleware for parsing application/json
+body-parser deprecidated, use express instead
+	depreciated
+	const bodyParser = require('body-parser');
+*/
+app.use(express.static(__dirname));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 routes(app);
 
